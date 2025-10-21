6ef3b4f85f293b7de7931b766eac3f5d1bd4e319
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-10-21 14:36:30 +0000

    int_orders_margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,6 +1,10 @@
-SELECT s.*,
-    p.purchase_price AS purchase_cost,
-    ROUND(s.revenue-p.purchase_price,2) AS margin
+SELECT s.date_date,
+    s.orders_id,
+    s.products_id,
+    s.revenue AS revenue,
+    s.quantity AS quantity,
+    p.purchase_price*s.quantity AS purchase_cost,
+    s.revenue-(p.purchase_price*s.quantity) AS margin
 FROM {{ref("stg_raw__sales")}} AS s
 LEFT JOIN {{ref("stg_raw__product")}} AS p 
     USING(products_id)
