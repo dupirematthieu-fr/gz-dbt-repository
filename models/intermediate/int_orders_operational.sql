61a5caac8f98162cf5a5b42420c724194232903d
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-10-21 15:23:27 +0000

    mrt_finance_days

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -3,7 +3,10 @@ SELECT m.orders_id,
     ROUND(m.margin+s.shipping_fee-s.log_cost-s.ship_cost,2) AS operational_margin,
     m.quantity,
     m.purchase_cost,
-    m.margin
+    m.margin,
+    m.revenue,
+    s.shipping_fee,
+    s.log_cost
 FROM {{ref("int_orders_margin")}} AS m
 LEFT JOIN {{ref("stg_raw__ship")}} AS s
     USING(orders_id)
