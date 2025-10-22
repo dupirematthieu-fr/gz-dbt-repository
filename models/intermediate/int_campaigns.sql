0cac408e3aac1b35d5100e20f6ee23ad913d6089
DUPIRE Matthieu <dupire.matthieu@gmail.com>
2025-10-22 14:55:05 +0000

    updated with package

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,12 +1,4 @@
-SELECT *
-FROM {{ref("stg_raw__adwords")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__bing")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__criteo")}}
-UNION ALL
-SELECT *
-FROM {{ref("stg_raw__facebook")}}
-ORDER BY date_date
+{{ dbt_utils.union_relations(
+    relations=[ref('stg_raw__adwords'), ref('stg_raw__bing'), ref('stg_raw__criteo'), ref('stg_raw__facebook')],
+    exclude=["_loaded_at"]
+) }}
